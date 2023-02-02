e8e4426f4ed520b18daacf7f86c2963ddffedcbb
Juan Mirarchi <mirarchi.juan18@gmail.com>
2023-02-01 21:50:58 -0300

    detalles finales 2

--- js.js/script.js
+++ js.js/script.js
@@ -15,17 +15,18 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+
 //Funcion que aplica las animaciones de las habilidades
 function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("adobepremier");
+        habilidades[1].classList.add("aftereffects");
+        habilidades[2].classList.add("photoshoplightroom");
+        habilidades[3].classList.add("adobeiilustrator");
+        habilidades[4].classList.add("htmlcssjs");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
